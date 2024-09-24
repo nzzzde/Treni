c91836113f19cf3928a1c2e049f3b5db3722b208
Illya Kucheruk <Illia.Kucheruk.KB.2023@lpnu.ua>
2024-09-25 00:53:35 +0300

    added bmi calculation to consol version

--- console/bmi.py
+++ console/bmi.py
@@ -1,1 +1,18 @@
-#bmi
+weight = float(input("Введіть вагу: "))					#User enters weight
+height = float(input("Введіть ріст в см: "))/100		#User enters height
+
+if not (0 <= height < 3 and 0 <= weight <= 300):		#Checking the correctness of variables
+	raise ValueError("Будь ласка, введіть реалістичні значення зросту та ваги.")
+
+bmi_value = weight / (height ** 2)						#Calculation of BMI
+print(bmi_value)										#Derivation of BMI
+
+#It is necessary to add recommendations according to the result of BMI
+if bmi_value < 18.5:				#Analysis of BMI
+		print("Недостатня вага")		
+elif 18.5 <= bmi_value > 24.9:
+        print("Нормальна вага")
+elif 25 <= bmi_value > 29.9:
+        print("Надмірна вага")
+else:
+        print("Ожиріння")
