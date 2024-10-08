e7810db363ce561e09de74b9980b7a83f227fa47
Illya Kucheruk <Illia.Kucheruk.KB.2023@lpnu.ua>
2024-10-08 17:11:54 +0300

    додав можливість користувачу обрати сет тренувань

--- console/bmi.py
+++ console/bmi.py
@@ -1,10 +1,23 @@
 import random
 
+
+def switch_case_set(set):
+    if set == 1:
+        return "зниження ваги"
+    elif set == 2:
+        return "підтримки ваги"
+    elif set == 3:
+        return "набору ваги"
+    else:
+        raise ValueError("Помилка з обраним сетом тренувань...")
+
+
 weight = float(input("Введіть вагу: "))                    # User enters weight
 height = float(input("Введіть ріст в см: ")) / 100        # User enters height
 
 if not (0 <= height < 3 and 0 <= weight <= 300):           # Checking the correctness of variables
-    raise ValueError("Будь ласка, введіть реалістичні значення зросту та ваги.")
+    raise ValueError(
+        "Будь ласка, введіть реалістичні значення зросту та ваги.")
 
 bmi_value = weight / (height ** 2)                         # Calculation of BMI
 print(bmi_value)                                            # Derivation of BMI
@@ -31,7 +44,7 @@ elif bmi_value >= 25 and bmi_value < 29.9:                  # Recommendations fo
         "Ваш ІМТ є трохи завищеним. Рекомендуємо вам план тренувань «зниження ваги»."
     ]
     print(random.choice(recommendations_overweight))
-else: 
+else:
     recommendations_obesity = [
         "Ваш ІМТ свідчить про ожиріння. Рекомендуємо звернутись до спеціаліста та обрати план «зниження ваги».",
         "У вас ІМТ, що вказує на ожиріння. Рекомендуємо обрати план тренувань «зниження ваги».",
@@ -39,3 +52,19 @@ else:
     ]
     print(random.choice(recommendations_obesity))
 
+# пропонуємо користувачу вибрати сет тренувань для: скидання/підтримування/набір ваги
+if bmi_value < 18.5:
+    print("За результатами ІМТ ми пропонуємо сет тренувань для «набору ваги»")
+    set = 3
+elif bmi_value >= 18.5 and bmi_value < 24.9:
+    print("За результатами ІМТ ми пропонуємо сет тренувань для «зниження ваги» «підтримки ваги» «набору ваги»")
+    set = int(input("Виберіть що вас цікавить(зниження-1 підтримка-2 набір-3): "))
+    if not (set == 1 or set == 2 or set == 3):           # Checking the correctness of variables
+        raise ValueError("Ви ввели не коректну команду...")
+else:
+    print("За результатами ІМТ ми пропонуємо сет тренувань для «зниження ваги» або «підтримки ваги» ")
+    set = int(input("Виберіть що вас цікавить(зниження-1 підтримка-2): "))
+    if not (set == 1 or set == 2):           # Checking the correctness of variables
+        raise ValueError("Ви ввели не коректну команду...")
+
+print(f"Ви обрали cет тренувань для {switch_case_set(set)}")
